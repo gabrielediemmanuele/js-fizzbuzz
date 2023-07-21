f933d04019a3daad654c5eee5a1672552709ca34
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-21 16:21:31 +0200

    feat: js print number 1 to 100

---
+++ main.js
@@ -0,0 +1,10 @@
+//Dire al programma che per i
+//di stampare in console numeri da 1 a 100
+
+for (let number = 1; number <= 100; number++) {
+  console.log(number);
+  //nuova variabile per il number
+  let result = number;
+
+  //Condizioni multipli di 3 & 5, 3 e dopo 5.
+}
