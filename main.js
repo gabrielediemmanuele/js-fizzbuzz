24a65667ffcdcb041421ee9aba5bfd67c5e7161e
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-07-21 17:00:19 +0200

    feat: add boxes

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//Container dei quadrotti...
+const container = document.getElementById("boxcontainer");
+
 //Dire al programma che per i
 //di stampare in console numeri da 1 a 100
 
@@ -8,24 +11,22 @@ for (let number = 1; number <= 100; number++) {
 
   //Condizioni multipli di 3 & 5, 3 e dopo 5 con il ciclo.
   if (number % 3 == 0 && number % 5 == 0) {
-    result = "FizzBuzz;";
+    result = "FizzBuzz";
   } else if (number % 3 == 0) {
     result = "Fizz";
   } else if (number % 5 == 0) {
     result = "Buzz";
   }
   console.log(result);
+
+  const boxes = document.createElement("div");
+
+  boxes.innerHTML = result;
+  boxes.classList.add("boxes");
+  container.append(boxes);
 }
 
 // Si devono calcolare prima i multipli di 3 e 5
 // perché una volta trovati la sequenza continua.
 // altrimenti trovando solo quelli del 3, o del 5
 // si fermerebbe.
-
-const container = document.getElementById("boxcontainer");
-
-const boxes = document.createElement("div");
-
-boxes.innerHTML = result;
-boxes.classList.add("boxes");
-container.append(boxes);
